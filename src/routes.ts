7919c55bd13153f77480aa8301415ca3f2957456
bitomic <35199700+bitomic@users.noreply.github.com>
2023-02-17 17:35:34 -0700

    feat: utility to get routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,3 +3,18 @@ export enum Routes {
 	CONFIGURATIONS = 'configurations/:guildId',
 	GUILDS = 'guilds/:guildId'
 }
+
+type Replacer<K extends string> = Record<K, string>
+
+export function getRoute( route: Routes.CHANNELS, replace: Replacer<'guildId'> ): string
+export function getRoute( route: Routes.CONFIGURATIONS, replace: Replacer<'guildId'> ): string
+export function getRoute( route: Routes.GUILDS, replace: Replacer<'guildId'> ): string
+export function getRoute( route: Routes, replace: Replacer<string> | undefined = undefined ): string {
+	let result: string = route
+	if ( replace ) {
+		for ( const [ key, value ] of Object.entries( replace ) ) {
+			result = result.replace( key, value )
+		}
+	}
+	return result
+}
