c33f5c57f8edd08ae7903c27f8a7f93906182f0b
bitomic <35199700+bitomic@users.noreply.github.com>
2023-02-17 21:09:21 -0600

    fix: add wiki to profiles get response

--- src/endpoints/profiles.ts
+++ src/endpoints/profiles.ts
@@ -24,6 +24,7 @@ export type ProfilesGETResponse = MaybeError<Array<{
 	color?: number
 	name?: string
 	type: ProfileType
+	wiki: string
 }>>
 
 export const ProfilesGETValidator = s.object<ProfilesGETRequest>( {
