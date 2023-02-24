1567e888e6d7bc82dafb9389d666de2cbda9e857
bitomic <35199700+bitomic@users.noreply.github.com>
2023-02-24 15:08:44 -0700

    fix: add follow route

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,14 @@
 export enum Routes {
 	CHANNELS = 'channels/:guildId',
 	CONFIGURATIONS = 'configurations/:guildId',
+	FOLLOW = 'follow/:guildId',
 	GUILDS = 'guilds/:guildId',
 	PROFILES = 'profiles/:guildId'
 }
 
 type Replacer<K extends string> = Record<K, string>
 
-export function getRoute( route: Routes.CHANNELS | Routes.CONFIGURATIONS | Routes.GUILDS | Routes.PROFILES, replace: Replacer<'guildId'> ): string
+export function getRoute( route: Routes.CHANNELS | Routes.CONFIGURATIONS | Routes.FOLLOW | Routes.GUILDS | Routes.PROFILES, replace: Replacer<'guildId'> ): string
 export function getRoute( route: Routes, replace: Replacer<string> | undefined = undefined ): string {
 	let result: string = route
 	if ( replace ) {
