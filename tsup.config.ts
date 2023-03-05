4bb9e55592b4d49e8c15cce0daaefd87bcedc197
bitomic <35199700+bitomic@users.noreply.github.com>
2023-03-05 11:57:27 -0600

    chore: disable dts in tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig( {
 	clean: true,
-	dts: true,
+	dts: false,
 	entry: [ 'src/**/*.ts', '!src/**/*.d.ts' ],
 	format: [ 'cjs', 'esm' ],
 	minify: true,
